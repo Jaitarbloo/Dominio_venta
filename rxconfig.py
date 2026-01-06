518966c29f0450260e35679ebf1f6271f7014b26
Jaitarbloo <jaitarbloo@yahoo.es>
2026-01-06 22:47:29 +0100

    AÃ±adiendo mejoras y componentes

--- rxconfig.py
+++ rxconfig.py
@@ -1,8 +1,18 @@
 import reflex as rx
 
 config = rx.Config(
-    app_name="Dominio_venta",
-    plugins=[
+                    
+                    app_name="Dominio_venta",
+                    
+                    api_url="https://dominio.onrender.com",
+    
+                   			cors_allowed_origins=[ "http://localhost:3000",
+        
+                                          "https://www.zabalgana.com",
+        
+                                        			],
+                    
+                    plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
     ]
